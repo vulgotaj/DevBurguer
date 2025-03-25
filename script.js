4035368075a56ed0fffc63b85f0e3eede7342b5c
Felipe Tajima <felipebtajima05@gmail.com>
2025-03-24 21:00:37 -0300

    Adding Functions to the Cart

--- script.js
+++ script.js
@@ -1,1 +1,122 @@
-console.log("TESTE TESTE");
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("adress-warn");
+
+let cart = [];
+
+// Abrir Modal do Carrinho
+cartBtn.addEventListener("click", function() {
+    updateCartModal();
+    cartModal.style.display = "flex";
+});
+
+// Fechar Modal quando clickar fora
+cartModal.addEventListener("click", function(event) {
+    if(event.target === cartModal) {
+        cartModal.style.display = "none";
+    }
+});
+
+closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none";
+});
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn");
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+
+        //Adicionar ao carrinho.
+        addToCart(name, price);
+    }
+})
+
+//Função Add to Cart
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name);
+
+    if(existingItem) {
+        //Se o item já existe, aumenta a quantidade
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+    }
+
+    updateCartModal()
+}
+
+//Atualiza o carrinho
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+            <div class= "flex items-center justify-between">
+                <div>
+                    <p class="font-normal">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-normal mt-2"> R$ ${item.price.toFixed(2)}</p>
+                </div>
+                
+                <button class="remove-from-cart-btn" data-name="${item.name}">
+                    Remover
+                </button>
+               
+            </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+};
+
+// Função para remover o item do carrinho
+cartItemsContainer.addEventListener("click", function(event) {
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1) {
+        const item = cart[index];
+
+        if(item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
